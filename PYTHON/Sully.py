a3a6f3a7fbccf256bb9d789a695e3ffc20d6f8b1
nirae <hello@nicolasdubouilh.fr>
2020-06-12 20:40:00 +0200

    Sully python

---
+++ PYTHON/Sully.py
@@ -0,0 +1,27 @@
+#! /usr/bin/python3
+
+import os
+
+source = """#! /usr/bin/python3
+
+import os
+
+source = {q}{q}{q}{s}{q}{q}{q}
+
+number = {nb}
+
+if number > 0:
+    file = open("Sully_%d.py" % number, "w+")
+    file.write(source.format(q='"', s=source, nb=(number - 1)))
+    file.close()
+    os.chmod("Sully_%d.py" % number, 0o755)
+    os.system("./Sully_%d.py" % number)"""
+
+number = 5
+
+if number > 0:
+    file = open("Sully_%d.py" % number, "w+")
+    file.write(source.format(q='"', s=source, nb=(number - 1)))
+    file.close()
+    os.chmod("Sully_%d.py" % number, 0o755)
+    os.system("./Sully_%d.py" % number)
