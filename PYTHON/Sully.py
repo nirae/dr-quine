6d645ac40bc8957099dc247c2c438266e1ba837a
nirae <hello@nicolasdubouilh.fr>
2020-06-14 23:17:06 +0200

    cleaning

--- PYTHON/Sully.py
+++ PYTHON/Sully.py
@@ -10,19 +10,19 @@ source = {q}{q}{q}{s}{q}{q}{q}
 
 number = {nb}
 
+file = open("Sully_%d.py" % number, "w+")
+file.write(source.format(q='"', s=source, nb=(number - 1)))
+file.close()
+os.chmod("Sully_%d.py" % number, 0o755)
 if number > 0:
-    file = open("Sully_%d.py" % number, "w+")
-    file.write(source.format(q='"', s=source, nb=(number - 1)))
-    file.close()
-    os.chmod("Sully_%d.py" % number, 0o755)
     os.system("./Sully_%d.py" % number)
 """
 
 number = 5
 
+file = open("Sully_%d.py" % number, "w+")
+file.write(source.format(q='"', s=source, nb=(number - 1)))
+file.close()
+os.chmod("Sully_%d.py" % number, 0o755)
 if number > 0:
-    file = open("Sully_%d.py" % number, "w+")
-    file.write(source.format(q='"', s=source, nb=(number - 1)))
-    file.close()
-    os.chmod("Sully_%d.py" % number, 0o755)
     os.system("./Sully_%d.py" % number)
