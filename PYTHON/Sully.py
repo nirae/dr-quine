f0aaef08076cfb4561542b4c4713a02d5edaa72e
nirae <hello@nicolasdubouilh.fr>
2020-06-18 12:20:48 +0200

    Fix Sully decrement

--- PYTHON/Sully.py
+++ PYTHON/Sully.py
@@ -10,19 +10,21 @@ source = {q}{q}{q}{s}{q}{q}{q}
 
 number = {nb}
 
-file = open("Sully_%d.py" % number, "w+")
-file.write(source.format(q='"', s=source, nb=(number - 1)))
-file.close()
-os.chmod("Sully_%d.py" % number, 0o755)
-if number > 0:
-    os.system("./Sully_%d.py" % number)
+if number >= 0:
+    file = open("Sully_%d.py" % number, "w+")
+    file.write(source.format(q='"', s=source, nb=(number - 1 if number else 0)))
+    file.close()
+    os.chmod("Sully_%d.py" % number, 0o755)
+    if number > 0:
+        os.system("./Sully_%d.py" % number)
 """
 
 number = 5
 
-file = open("Sully_%d.py" % number, "w+")
-file.write(source.format(q='"', s=source, nb=(number - 1)))
-file.close()
-os.chmod("Sully_%d.py" % number, 0o755)
-if number > 0:
-    os.system("./Sully_%d.py" % number)
+if number >= 0:
+    file = open("Sully_%d.py" % number, "w+")
+    file.write(source.format(q='"', s=source, nb=(number - 1 if number else 0)))
+    file.close()
+    os.chmod("Sully_%d.py" % number, 0o755)
+    if number > 0:
+        os.system("./Sully_%d.py" % number)
